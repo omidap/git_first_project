aba4b0def42b8a8c65bc4acf33db3c2a14c363d1
omdid <omid.rezaey@tekschool.us>
2023-08-11 14:16:06 -0400

    Adding first git project

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+G
+src/demo/TestClass.java,9\b\9bce35d5915e515075c4df8d706abeff96b5b04e
---
+++ src/demo/TestClass.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello guardians!");
+    }
+}
